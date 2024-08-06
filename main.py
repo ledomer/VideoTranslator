ab8e9a8447632020e49debb9926d0123a0225856
Andrey Gusyatnikov <gusyatnikov.a@gmail.com>
2024-08-06 20:44:58 +0300

    deleted test code

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import re
 
+
 def main():
     with open(file='Lecture 4. Create the Project_EN.srt', mode='r', encoding='utf-8') as fsub:
         content = fsub.read()
         result = re.findall(r'(\d)\n((?:\d\d:?){3},\d{3})\s*-->\s*((?:\d\d:?){3},\d{3})\n(.+)\n', content)
         for (num, date_from, date_to, text) in result:
             print(int(num), date_from, date_to, text)
-
 
 
 if __name__ == '__main__':
